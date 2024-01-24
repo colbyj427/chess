d834434057d8e9555bc38141448562ab042811b5
colbyj427 <colbyj427@gmail.com>
2024-01-23 21:36:21 -0700

    Final committ, all tests passed.

--- chess/shared/src/main/java/chess/ChessPiece.java
+++ chess/shared/src/main/java/chess/ChessPiece.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -16,6 +17,18 @@ public class ChessPiece {
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ChessPiece that)) return false;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
