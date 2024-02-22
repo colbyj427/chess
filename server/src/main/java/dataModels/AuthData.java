8a750655265a651f04cb61e71f3c4f0c2152949d
colbyj427 <colbyj427@gmail.com>
2024-02-21 20:01:40 -0700

    added getters and setters to dataModels.

--- server/src/main/java/dataModels/AuthData.java
+++ server/src/main/java/dataModels/AuthData.java
@@ -7,9 +7,25 @@ public class AuthData {
   private String authToken;
   private String userName;
 
-
   public AuthData(final String authToken, final String userName) {
     this.authToken=authToken;
     this.userName = userName;
   }
+  public String getAuthToken() {
+    return authToken;
+  }
+
+  public void setAuthToken(String authToken) {
+    this.authToken=authToken;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName=userName;
+  }
+
+
 }
