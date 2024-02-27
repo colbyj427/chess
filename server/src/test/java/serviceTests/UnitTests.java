ef4491a2b5ae2550d1e215061f471684bb4c69a5
colbyj427 <colbyj427@gmail.com>
2024-02-26 18:55:01 -0700

    Created unit tests for each service class method.

--- server/src/test/java/serviceTests/UnitTests.java
+++ server/src/test/java/serviceTests/UnitTests.java
@@ -6,6 +6,7 @@ import model.GameRecord;
 import model.UserRecord;
 import org.junit.jupiter.api.*;
 import service.ClearService;
+import service.GameService;
 import service.UserService;
 
 import java.util.ArrayList;
@@ -30,7 +31,9 @@ public class UnitTests {
   public void userServiceSuccessfulRegister() {
     UserRecord newUser = new UserRecord("username", "password", "email@gmail.com");
     UserService userService = new UserService();
+    ClearService clearService = new ClearService();
     Assertions.assertDoesNotThrow(() -> {
+      clearService.clear();
       userService.register(newUser.username(), newUser.password(), newUser.email());
     });
   }
@@ -66,8 +69,10 @@ public class UnitTests {
   public void userServiceSuccessfulLogout() {
     UserRecord newUser = new UserRecord("username", "password", "email@gmail.com");
     UserService userService = new UserService();
+    ClearService clearService = new ClearService();
 
     Assertions.assertDoesNotThrow(() -> {
+      clearService.clear();
       AuthRecord authRecord = userService.register(newUser.username(), newUser.password(), newUser.email());
       userService.logout(authRecord.authToken());
     });
@@ -82,4 +87,84 @@ public class UnitTests {
       userService.logout("InvalidAuthToken");
     });
   }
+  @Test
+  public void gameServiceSuccessfulCreateGame() {
+    UserRecord newUser = new UserRecord("username", "password", "email@gmail.com");
+    UserService userService = new UserService();
+    GameService gameService = new GameService();
+    ClearService clearService = new ClearService();
+
+    Assertions.assertDoesNotThrow(() -> {
+      clearService.clear();
+      AuthRecord authRecord = userService.register(newUser.username(), newUser.password(), newUser.email());
+      gameService.createGame(authRecord.authToken(), "1");
+    });
+  }
+  @Test
+  public void gameServiceBadCreateGame() {
+    UserRecord newUser = new UserRecord("username", "password", "email@gmail.com");
+    UserService userService = new UserService();
+    GameService gameService = new GameService();
+
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      AuthRecord authRecord = userService.register(newUser.username(), newUser.password(), newUser.email());
+      gameService.createGame("InvalidAuthToken", "1");
+    });
+  }
+  @Test
+  public void gameServiceSuccessfulListGames() {
+    UserRecord newUser = new UserRecord("username", "password", "email@gmail.com");
+    UserService userService = new UserService();
+    GameService gameService = new GameService();
+    ClearService clearService = new ClearService();
+
+    Assertions.assertDoesNotThrow(() -> {
+      clearService.clear();
+      AuthRecord authRecord = userService.register(newUser.username(), newUser.password(), newUser.email());
+      gameService.createGame(authRecord.authToken(), "1");
+      gameService.listGames(authRecord.authToken());
+    });
+  }
+  @Test
+  public void gameServiceBadListGames() {
+    UserRecord newUser = new UserRecord("username", "password", "email@gmail.com");
+    UserService userService = new UserService();
+    GameService gameService = new GameService();
+
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      AuthRecord authRecord = userService.register(newUser.username(), newUser.password(), newUser.email());
+      gameService.createGame(authRecord.authToken(), "1");
+      gameService.listGames("InvalidAuthToken");
+    });
+  }
+  @Test
+  public void gameServiceSuccessfulJoinGame() {
+    UserRecord newUser = new UserRecord("username", "password", "email@gmail.com");
+    UserService userService = new UserService();
+    GameService gameService = new GameService();
+    ClearService clearService = new ClearService();
+
+    Assertions.assertDoesNotThrow(() -> {
+      clearService.clear();
+      AuthRecord authRecord = userService.register(newUser.username(), newUser.password(), newUser.email());
+      GameRecord gameRecord = gameService.createGame(authRecord.authToken(), "1");
+      gameService.joinGame(authRecord.authToken(), "WHITE", gameRecord.gameID());
+    });
+  }
+  @Test
+  public void gameServiceBadJoinGame() {
+    UserRecord newUser = new UserRecord("username", "password", "email@gmail.com");
+    UserService userService = new UserService();
+    GameService gameService = new GameService();
+
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      AuthRecord authRecord = userService.register(newUser.username(), newUser.password(), newUser.email());
+      gameService.createGame(authRecord.authToken(), "1");
+      gameService.joinGame(authRecord.authToken(), "WHITE", 2);
+    });
+  }
+//  public void clearAll() {
+//    ClearService clearService = new ClearService();
+//    clearService.clear();
+//  }
 }
