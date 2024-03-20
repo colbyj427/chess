cb689fee746bc96d7b8601057e9f3597b5b7b9c5
colbyj427 <colbyj427@gmail.com>
2024-03-20 12:54:09 -0600

    wrote tests for each endpoint in server facade.

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,7 +1,10 @@
 package clientTests;
 
 import ServerClientCommunication.ServerFacade;
+import dataAccess.DataAccessException;
 import model.AuthRecord;
+import model.GameRecord;
+import model.JoinGameRecord;
 import model.UserRecord;
 import org.junit.jupiter.api.*;
 import server.Server;
@@ -30,13 +33,111 @@ public class ServerFacadeTests {
         ServerFacade.clear(null);
     }
     @Test
-    public void sampleTest() {
-        assertTrue(true);
-    }
-    @Test
     void register() throws Exception {
         UserRecord request = new UserRecord("username", "password", "email");
         AuthRecord authData = ServerFacade.register(request);
         assertTrue(authData.authToken().length() > 10);
+    }
+    @Test
+    void badRegister() throws Exception {
+        UserRecord request = new UserRecord(null, "password", "email");
+        Assertions.assertThrows(Exception.class, () -> {
+            AuthRecord authData=ServerFacade.register(request);
+        });
+    }
+    @Test
+    public void login() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        ServerFacade.logout(null, registerAuthData.authToken());
+        UserRecord loginRequest = new UserRecord("username", "password", null);
+        AuthRecord loginAuthData = ServerFacade.login(loginRequest);
+        assertTrue(loginAuthData.authToken().length() > 10);
+    }
+    @Test
+    public void badLogin() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        ServerFacade.logout(null, registerAuthData.authToken());
+        UserRecord loginRequest = new UserRecord("wrongusername", "password", null);
+        Assertions.assertThrows(Exception.class, () -> {
+            AuthRecord authData=ServerFacade.login(loginRequest);
+        });
+    }
+    @Test
+    public void logout() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        Assertions.assertDoesNotThrow(() -> {
+        ServerFacade.logout(null, registerAuthData.authToken());
+        });
+    }
+    @Test
+    public void badLogout() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+
+        Assertions.assertThrows(Exception.class, () -> {
+            ServerFacade.logout(null, "badAuthToken");
+        });
+    }
+    @Test
+    public void createGame() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        GameRecord newGame = new GameRecord(0, null, null, "newgame", null, null);
+        GameRecord createdGame = ServerFacade.createGame(newGame, registerAuthData.authToken());
+        assertTrue(createdGame.gameName().equals("newgame"));
+    }
+    @Test
+    public void badCreateGame() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        GameRecord newGame = new GameRecord(0, null, null, "newgame", null, null);
+        Assertions.assertThrows(Exception.class, () -> {
+            ServerFacade.createGame(newGame, "badAuthToken");
+        });
+    }
+    @Test
+    public void joinGame() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        GameRecord newGame = new GameRecord(0, null, null, "newgame", null, null);
+        GameRecord createdGame = ServerFacade.createGame(newGame, registerAuthData.authToken());
+        JoinGameRecord joinRecord = new JoinGameRecord("BLACK", createdGame.gameID());
+        Assertions.assertDoesNotThrow(() -> {
+            ServerFacade.joinGame(joinRecord, registerAuthData.authToken());
+        });
+    }
+    @Test
+    public void BadjoinGame() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        GameRecord newGame = new GameRecord(0, null, null, "newgame", null, null);
+        GameRecord createdGame = ServerFacade.createGame(newGame, registerAuthData.authToken());
+        JoinGameRecord joinRecord = new JoinGameRecord("ORANGE", createdGame.gameID());
+        Assertions.assertThrows(Exception.class, () -> {
+            ServerFacade.joinGame(joinRecord, registerAuthData.authToken());
+        });
+    }
+    @Test
+    public void listGames() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        GameRecord newGame = new GameRecord(0, null, null, "newgame", null, null);
+        ServerFacade.createGame(newGame, registerAuthData.authToken());
+        Assertions.assertDoesNotThrow(() -> {
+            ServerFacade.listGames(null, registerAuthData.authToken());
+        });
+    }
+    @Test
+    public void badListGames() throws Exception{
+        UserRecord registerRequest = new UserRecord("username", "password", "email");
+        AuthRecord registerAuthData = ServerFacade.register(registerRequest);
+        GameRecord newGame = new GameRecord(0, null, null, "newgame", null, null);
+        ServerFacade.createGame(newGame, registerAuthData.authToken());
+        Assertions.assertThrows(Exception.class, () -> {
+            ServerFacade.listGames(null, "badAuthToken");
+        });
     }
 }
