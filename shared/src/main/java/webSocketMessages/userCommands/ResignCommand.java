341dec7e9c8a7134411f1909370f6be14f41afe4
colbyj427 <colbyj427@gmail.com>
2024-04-06 13:58:54 -0600

    recommit after makemove, all files said i changed them for some reason?

--- shared/src/main/java/webSocketMessages/userCommands/ResignCommand.java
+++
@@ -1,9 +0,0 @@
-package webSocketMessages.userCommands;
-
-public class ResignCommand extends UserGameCommand {
-  int gameId;
-  public ResignCommand(String authToken, String username, CommandType type, int gameId) {
-    super(authToken, username, type);
-    this.gameId = gameId;
-  }
-}
